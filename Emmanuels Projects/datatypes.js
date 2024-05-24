cb389be62cfc88f0baa2bd5437643c9a7259aa7c
PrinxPerry <158264548+PrinxPerry@users.noreply.github.com>
2024-05-24 08:39:12 +0100

    Add files via upload

---
+++ Emmanuels Projects/datatypes.js
@@ -0,0 +1,8 @@
+function toogleDropdown () {
+    var dropdown = document.getElementById("myDropdown");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
